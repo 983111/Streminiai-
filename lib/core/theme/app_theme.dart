5f895220513160b05af3334e39b810af7ad27d7a
Vishwajeet <vishwajeetadkine705@gmail.com>
2026-01-11 17:17:05 +0530

    Add imports for colors and text styles in AppTheme

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+import 'app_text_styles.dart';
+
 class AppTheme {
   static ThemeData get darkTheme {
     return ThemeData.dark().copyWith(
